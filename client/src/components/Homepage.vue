eb1cce2257daefe6ca71443e76d4a1e19950c2a1
Mark Venuto <markvenutobass@gmail.com>
2022-10-12 22:46:08 -0400

    Started working on workout app

---
+++ client/src/components/Homepage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <p>hello</p>
+  </div>
+</template>
+
+<style scoped>
+  p {
+    color: var(--white);
+  }
+</style>
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar is-dark has-shadow has-text-weight-light" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                <img src="../assets/logo.png">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="myNavbar" class="navbar-menu">
+            <div class="navbar-start">
+                <div class="navbar-item">
+                    <a href="/#" class="button is-success text">
+                        Add a workout +
+                    </a>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        My Account
+                    </a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item" href="#">
+                            Profile
+                        </a>
+                        <a class="navbar-item">
+                            Friends
+                        </a>
+                        <a class="navbar-item">
+                            Settings
+                        </a>
+                    </div>
+                </div>
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-dark">
+                            Log in
+                        </a>
+                        <a class="button is-light">
+                            <strong>Sign up</strong>
+                        </a>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </nav>
+</template>
