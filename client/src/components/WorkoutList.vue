4c855c5f6bfbb4321161155738185b331f2744c5
Mark Venuto <markvenutobass@gmail.com>
2022-10-15 11:27:37 -0400

    can now delete workouts

--- client/src/components/WorkoutList.vue
+++ client/src/components/WorkoutList.vue
@@ -1,12 +1,29 @@
 <!-- TODO: should take in a userid and lists their workouts. this allows user to look at friend's previous workouts -->
 
-<script setup lang="ts">
+<script lang="ts">
+import { ref } from "vue";
 import WorkoutRecord from "./WorkoutRecord.vue";
 import { useWorkoutsStore } from "../stores/workouts";
 
-const workouts = useWorkoutsStore().workouts;
-
+export default {
+  data() {
+    return {
+      workouts: useWorkoutsStore().workouts,
+    };
+  },
+  components: {
+    WorkoutRecord,
+  },
+  methods: {
+    // Deletes workout from list
+    deleteWorkout(id: number) {
+      useWorkoutsStore().deleteWorkout(id);
+      this.workouts = useWorkoutsStore().workouts;
+    },
+  },
+};
 </script>
+
 <!-- the box containing the list of previous workouts -->
 <template>
   <div
@@ -16,7 +33,15 @@ const workouts = useWorkoutsStore().workouts;
       <div class="content is-flex is-flex-direction-column">
         <h3 class="has-text-white">date1-date2</h3>
       </div>
-      <WorkoutRecord v-for="workout in workouts" :date='workout.date' :exercises='workout.exercises'/>
+      <WorkoutRecord 
+        v-if="workouts.length > 0"
+        v-for="workout in workouts"
+        :id="workout.id"
+        :date="workout.date"
+        :exercises="workout.exercises"
+        @delete="deleteWorkout"
+      />
+      <h3 class="title" v-else>Click "Add a workout"</h3>
     </div>
     <div
       id="workoutListOptions"
@@ -39,26 +64,28 @@ const workouts = useWorkoutsStore().workouts;
 <style scoped>
 h3 {
   padding: 1rem;
+  color: var(--white);
+  text-align: center;
 }
 
 #workoutList {
+  margin-bottom: auto;
   background-color: var(--navy-blue-dark);
   border: 1px solid var(--white);
   border-radius: 5px;
   padding: 0;
   min-width: 50%;
   width: 75%;
-  height: 75%;
+  max-height: 40vw;
   overflow-y: auto;
   margin-right: 10px;
 }
-
 #workoutListOptions {
+  margin-bottom: auto;
   max-width: 150px;
   margin-bottom: 10px;
   color: var(--white);
 }
-
 .field .input {
   width: 150px !important;
   margin: 0 8px 0 0;
--- client/src/components/WorkoutRecord.vue
+++ client/src/components/WorkoutRecord.vue
@@ -1,47 +1,53 @@
 <script lang="ts">
+import { ref } from "vue";
 
 export default {
-  props: ['date', 'exercises'],
-  mounted() {
-    // console.log(this.date);
-  }
+  data(){
+    return{
+        isActive: true
+    }    
+  },
+  props: ["id", "date", "exercises"],
+  methods: {
+    deleteWorkout() {
+      this.$emit("delete", this.id); // emit id of workout to be deleted
+      this.isActive = false; // stop rendering component
+    },
+  },
 };
-
 </script>
 
-
 <template>
-    <div class="block is-flex is-justify-content-space-between">
-        <a class="workoutButton">{{ date }} - {{ exercises.length }} exercises</a>
-        <div class="buttons is-flex is-flex-direction-row">
-            <a href="#" class="button is-danger">Delete</a>
-      </div>
+  <div v-if="isActive" :class="{ 'is-active': isActive }" class="block is-flex is-justify-content-space-between">
+    <a class="workoutButton">{{ date }} - {{ exercises.length }} exercises</a>
+    <div class="buttons is-flex is-flex-direction-row">
+      <a class="button is-danger" @click="deleteWorkout">Delete</a>
     </div>
+  </div>
 </template>
 
 <style scoped>
-    .block{
-        border-top: 1px solid var(--white);
-        min-height: 5rem;
-        margin: 0 !important;
-    }
-    .workoutButton {
-        display: block;
-        text-decoration: none;
-        width:300px;
-        height: 100%;
-        padding: 1.5rem 0;
-        text-align: center;
-        font-weight: bold;
-        font-size: 1.25rem;
-        color: var(--white); 
-    }
-    .workoutButton:hover {
-        text-decoration: none;
-        background-color: var(--navy-blue-light);
-    }
-    .button{
-        margin: 0 8px 0 0;
-    }
-    
+.block {
+  border-top: 1px solid var(--white);
+  min-height: 5rem;
+  margin: 0 !important;
+}
+.workoutButton {
+  display: block;
+  text-decoration: none;
+  width: 300px;
+  height: 100%;
+  padding: 1.5rem 0;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.25rem;
+  color: var(--white);
+}
+.workoutButton:hover {
+  text-decoration: none;
+  background-color: var(--navy-blue-light);
+}
+.button {
+  margin: 0 8px 0 0;
+}
 </style>
